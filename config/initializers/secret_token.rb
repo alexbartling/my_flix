72aacd5019c91851b4a040990e6a8ee844eaedcf
abart711 <bartlingalex@gmail.com>
2013-04-19 12:58:46 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyFlix::Application.config.secret_token = '621a4a91e81d05332f77572dd492265563c09c8f8636fd3712c150a6ce97ac3d66cf48fc7fcaf0a7d8a1feff22ddf8ff888e51b64ccea28f817a93c296142c34'
